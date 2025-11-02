afafb344400cd7289904ba797572bedbfcefd92b
stephveile <veile.steph@gmail.com>
2025-11-02 13:35:49 -0600

    Initial commit

---
+++ coffee-journal-backend/coffee-journal-backend/src/main/java/com/example/coffee_journal_backend/CoffeeJournalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffee_journal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeJournalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeJournalBackendApplication.class, args);
+	}
+
+}
---
+++ coffee-journal-backend/coffee-journal-backend/src/test/java/com/example/coffee_journal_backend/CoffeeJournalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffee_journal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeJournalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
