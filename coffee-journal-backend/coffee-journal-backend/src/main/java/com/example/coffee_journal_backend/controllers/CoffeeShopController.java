24efb0fa1854ba14bdaa76bc1a993a50942e2445
stephveile <veile.steph@gmail.com>
2025-11-08 18:40:48 -0600

    CRUD JSON format

--- coffee-journal-backend/coffee-journal-backend/src/main/java/com/example/coffee_journal_backend/controllers/CoffeeShopController.java
+++ coffee-journal-backend/coffee-journal-backend/src/main/java/com/example/coffee_journal_backend/controllers/CoffeeShopController.java
@@ -3,9 +3,13 @@ package com.example.coffee_journal_backend.controllers;
 import com.example.coffee_journal_backend.models.CoffeeShop;
 import com.example.coffee_journal_backend.repositories.CoffeeShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
+import java.awt.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/coffeeshops")
@@ -14,27 +18,44 @@ public class CoffeeShopController {
     @Autowired
     CoffeeShopRepository coffeeShopRepository;
 
-    // Get all coffee shops
+    // Retrieve all coffee shops
     // GET request to http://localhost:8080/api/coffeeshops
     @GetMapping("")
-    public Collection<CoffeeShop> getAllCoffeeShops() {
-        return coffeeShopRepository.findAll();
+    public ResponseEntity<?> getAllCoffeeShops() {
+        List<CoffeeShop> allCoffeeShops = coffeeShopRepository.findAll();
+        return new ResponseEntity<>(allCoffeeShops, HttpStatus.OK);
     }
 
     // Retrieve specific coffee shop
-    // GET request to http://localhost:8080/api/coffeeshops/details
-    @GetMapping("/details/{shopId}")
-    public CoffeeShop getCoffeeShopById(@PathVariable int shopId) {
-        return coffeeShopRepository.findById(shopId).orElse(null);
+    // GET request to http://localhost:8080/api/coffeeshops/details/{shopId}
+    @GetMapping(value="/details/{shopId}", produces=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getCoffeeShopById(@PathVariable int shopId) {
+        CoffeeShop coffeeShop = coffeeShopRepository.findById(shopId).orElse(null);
+        if (coffeeShop == null) {
+            return new ResponseEntity<>("Coffee shop not found.", HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(coffeeShop, HttpStatus.OK);
+        }
     }
 
     // Save new coffee shop
-    // Use query parameters
-    // POST request to http://localhost:8080/api/coffeeshops/new
-    @PostMapping("/add")
-    public String addNewCoffeeShop(@RequestParam String shopName, String shopAddress, String shopPhone, String shopHours) {
-        CoffeeShop newCoffeeShop = new CoffeeShop(shopName, shopAddress, shopPhone, shopHours);
-        coffeeShopRepository.save(newCoffeeShop);
-        return "Coffee Shop added: " + newCoffeeShop;
+    // POST request to http://localhost:8080/api/coffeeshops/add
+    @PostMapping(value="/add", consumes=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> addNewCoffeeShop(@RequestBody CoffeeShop coffeeShop) {
+        coffeeShopRepository.save(coffeeShop);
+        return new ResponseEntity<>(coffeeShop, HttpStatus.CREATED);
+    }
+
+    // Delete existing coffee shop
+    // DELETE request to http://localhost:8080/api/coffeeshops/delete/{shopId}
+    @DeleteMapping(value="/delete/{shopId}")
+    public ResponseEntity<?> deleteCoffeeShop(@PathVariable int shopId) {
+        CoffeeShop coffeeShop = coffeeShopRepository.findById(shopId).orElse(null);
+        if (coffeeShop == null) {
+            return new ResponseEntity<>("Coffee shop not found.", HttpStatus.NOT_FOUND);
+        } else {
+            coffeeShopRepository.deleteById(shopId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 }
