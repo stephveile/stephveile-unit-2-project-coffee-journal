8eda59c4963a79ae136202fcc7cd50573b373917
stephveile <veile.steph@gmail.com>
2025-11-13 09:09:47 -0600

    fetch entries setup

--- coffee-journal-frontend/src/context/DataContext.jsx
+++ coffee-journal-frontend/src/context/DataContext.jsx
@@ -1,29 +1,43 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
+import { User, CoffeeShop, Entry } from '../classes/Exports';
 
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
 
-    const [ isLoading, setIsLoading ] = userState(true);
+    const [ isLoading, setIsLoading ] = useState(true);
 
     const [ allUsers, setAllUsers ] = useState(null);
     const [ allEntries, setAllEntries ] = useState(null);
     const [ allShops, setAllShops ] = useState(null);
     const [ allAddRequests, setAllAddRequests ] = useState(null);
 
-    const fetchUsers = async () => {
+    const fetchEntries = async () => {
+        console.log("starting fetch");
 
-        const users = [];
+        const entries = [];
 
         try {
-            const response = await fetch('.../public/test-data/users.json');
-            const data = await response.json();
-            // console.log(data);
+            const response = await fetch('http://localhost:8080/api/entries');
 
-            data.forEach(user => {
-                let user = ner User
-            })
-        } catch {
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.message || `ERROR - Status ${response.status}`);
+            } else {
+
+                const data = await response.json();
+
+                data.forEach(entry => {
+                    let user = new User(entry.user.id, entry.user.userName, entry.user.userEmail, entry.user.Password, entry.user.userCity);
+                    let coffeeShop = new CoffeeShop(entry.coffeeShop.id, entry.CoffeeShop.shopName, entry.CoffeeShop.shopAddress, entry.CoffeeShop.shopPhone, entry.CoffeeShop.shopHours);
+                    let newEntry = new Entry(entry.id, coffeeShop, entry.drinkOrder, entry.rating, entry.review, entry.wouldRecommend, entry.visitDate, user);
+                    entries.push[newEntry];
+                });
+
+                setAllEntries(entries);
+            }
+
+        } catch(error) {
 
         } finally {
 
@@ -31,8 +45,14 @@ export const DataProvider = ({ children }) => {
     }
 
     useEffect(() => {
-        fetchUsers();
+        fetchEntries();
     }, []);
 
-    return <DataContext.Provider value={{}}>{ children }</DataContext.Provider>;
+    useEffect(() => {
+        if (allEntries !== null) {
+           setIsLoading(false); 
+        }
+    }, [allEntries]);
+
+    return <DataContext.Provider value={{isLoading, allEntries, fetchEntries}}>{ children }</DataContext.Provider>;
 }
